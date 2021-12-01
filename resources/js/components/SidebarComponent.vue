7affbedba005507fb5164b3eaf8da5c9f9cb6af6
Zahid Jameel <zahidxee@gmail.com>
2021-12-01 13:08:46 +0500

    Roles changes

--- resources/js/components/SidebarComponent.vue
+++ resources/js/components/SidebarComponent.vue
@@ -15,7 +15,7 @@
                 </router-link>
             </li>
 
-            <li v-show="isLegalWing || isSuperAdmin">
+            <li v-show="isLegalWing || isSuperAdmin || isAssistantOne">
                 <router-link class="side-menu" :to="{ name: 'admin.advices.index'}" :class="{'side-menu--active':activeMenu==='Advices'}">
                     <div class="side-menu__icon">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
@@ -24,7 +24,7 @@
                 </router-link>
             </li>
 
-            <li  v-show="isLegalWing || isSuperAdmin">
+            <li  v-show="isLegalWing || isSuperAdmin || isAssistantOne">
                 <router-link class="side-menu" :to="{ name: 'admin.court-cases.index'}" :class="{'side-menu--active':activeMenu==='CourtCase'}">
                     <div class="side-menu__icon">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
@@ -33,7 +33,7 @@
                 </router-link>
             </li>
 
-            <li  v-show="isLegalWing || isSuperAdmin">
+            <li  v-show="isLegalWing || isSuperAdmin || isAssistantOne">
                 <router-link class="side-menu" :to="{ name: 'admin.document-vettings.index'}" :class="{'side-menu--active':activeMenu==='DocumentVetting'}">
                     <div class="side-menu__icon">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
@@ -42,7 +42,7 @@
                 </router-link>
             </li>
 
-            <li  v-show="isLegalWing || isSuperAdmin">
+            <li  v-show="isLegalWing || isSuperAdmin || isAssistantTwo">
                 <router-link class="side-menu" :to="{ name: 'admin.complaints-redressals.index'}" :class="{'side-menu--active':activeMenu==='ComplaintRedressal'}">
                     <div class="side-menu__icon">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
@@ -51,7 +51,7 @@
                 </router-link>
             </li>
 
-            <li  v-show="isLegalWing || isSuperAdmin">
+            <li  v-show="isLegalWing || isSuperAdmin || isAssistantThree">
                 <router-link class="side-menu" :to="{ name: 'admin.blacklistings.index'}" :class="{'side-menu--active':activeMenu==='Blacklisting'}">
                     <div class="side-menu__icon">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
@@ -60,7 +60,7 @@
                 </router-link>
             </li>
 
-            <li  v-show="isLegalWing || isSuperAdmin">
+            <li  v-show="isLegalWing || isSuperAdmin || isSupritendent">
                 <router-link class="side-menu" :to="{ name: 'admin.violation-examinations.index'}" :class="{'side-menu--active':activeMenu==='ViolationExamination'}">
                     <div class="side-menu__icon">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
@@ -69,7 +69,7 @@
                 </router-link>
             </li>
 
-            <li v-show="isITWing || isSuperAdmin">
+            <li v-show="isITWing || isSuperAdmin || isAssistantThree">
                 <router-link class="side-menu" :to="{ name: 'admin.documents-uploadings.index'}" :class="{'side-menu--active':activeMenu==='DocumentUploading'}">
                     <div class="side-menu__icon">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
@@ -78,7 +78,7 @@
                 </router-link>
             </li>
 
-            <li v-show="isITWing || isSuperAdmin">
+            <li v-show="isITWing || isSuperAdmin || isAssistantThree">
                 <router-link class="side-menu" :to="{ name: 'admin.accounts.index'}" :class="{'side-menu--active':activeMenu==='Account'}">
                     <div class="side-menu__icon">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
@@ -87,7 +87,7 @@
                 </router-link>
             </li>
 
-            <li v-show="isITWing || isSuperAdmin">
+            <li v-show="isITWing || isSuperAdmin || isAssistantThree">
                 <router-link class="side-menu" :to="{ name: 'admin.trainings.index'}" :class="{'side-menu--active':activeMenu==='Training'}">
                     <div class="side-menu__icon">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
@@ -96,7 +96,7 @@
                 </router-link>
             </li>
 
-            <li v-show="isITWing || isSuperAdmin">
+            <li v-show="isITWing || isSuperAdmin || isAssistantThree">
                 <router-link class="side-menu" :to="{ name: 'admin.procuring-agencies.index'}" :class="{'side-menu--active':activeMenu==='procuringAgency'}">
                     <div class="side-menu__icon">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
@@ -105,7 +105,7 @@
                 </router-link>
             </li>
 
-            <li v-show="isAdminWing || isSuperAdmin">
+            <li v-show="isAdminWing || isSuperAdmin || isAccountOfficer">
                 <router-link class="side-menu" :to="{ name: 'admin.tender-fees.index'}" :class="{'side-menu--active':activeMenu==='TenderFee'}">
                     <div class="side-menu__icon">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
@@ -114,7 +114,7 @@
                 </router-link>
             </li>
 
-            <li v-show="isAdminWing || isSuperAdmin">
+            <li v-show="isAdminWing || isSuperAdmin || isDeputyDirector">
                 <router-link class="side-menu" :to="{ name: 'admin.inspections.index'}" :class="{'side-menu--active':activeMenu==='Inspection'}">
                     <div class="side-menu__icon">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
@@ -150,6 +150,24 @@ export default {
         },
         isAdminWing() {
             return this.$store.state.auth.user.role_id===4;
+        },
+        isDeputyDirector() {
+            return this.$store.state.auth.user.role_id===5;
+        },
+        isAccountOfficer() {
+            return this.$store.state.auth.user.role_id===6;
+        },
+        isSupritendent() {
+            return this.$store.state.auth.user.role_id===7;
+        },
+        isAssistantOne() {
+            return this.$store.state.auth.user.role_id===8;
+        },
+        isAssistantTwo() {
+            return this.$store.state.auth.user.role_id===9;
+        },
+        isAssistantThree() {
+            return this.$store.state.auth.user.role_id===10;
         },
     },
     created() {
